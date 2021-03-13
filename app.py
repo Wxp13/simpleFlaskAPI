165a407635ca1d0659096ad96406cb392ad64158
wxp13 <wxp13@psu.edu>
2021-03-13 01:46:47 -0500

    new files for flask app

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from datetime import datetime
+import re
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return content
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
